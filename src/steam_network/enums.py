e688b67b377f1b4a09b95b0bcc09e1708c2f890e
ABaumher <78619068+ABaumher@users.noreply.github.com>
2023-06-10 13:22:56 -0400

    Update enums.py
    Added back the To_TwoFactorMethod (i think it's still used), but now with proper type hints. Properly type hinted the helper function as well.

--- src/steam_network/enums.py
+++ src/steam_network/enums.py
@@ -110,9 +110,13 @@ class TwoFactorMethod(enum.IntEnum):
     Unknown = 4 
     #EmailConfirm = 5 #Does not exist? Likely something Steam thought about implementing and decided not to. if that changes, we can support it. 
     
-
-def _to_TwoFactorMethod(auth_enum: int, msg: Optional[str]) -> Tuple[TwoFactorMethod, str]:
-    if (auth_enum == EAuthSessionGuardType.k_EAuthSessionGuardType_None):
+def to_TwoFactorMethod(auth_enum : Union[CAuthentication_AllowedConfirmation, EAuthSessionGuardType]) -> TwoFactorMethod:
+    if (isinstance(auth_enum, CAuthentication_AllowedConfirmation)):
+        auth_enum = auth_enum.confirmation_type
+    ret_val, _ = _to_TwoFactorMethod(auth_enum, None)
+    return ret_val
+    
+def _to_TwoFactorMethod(auth_enum : EAuthSessionGuardType, msg: Optional[str]) -> Tuple[TwoFactorMethod, str]:    if (auth_enum == EAuthSessionGuardType.k_EAuthSessionGuardType_None):
         return (TwoFactorMethod.Nothing, msg)
     elif (auth_enum == EAuthSessionGuardType.k_EAuthSessionGuardType_EmailCode):
         return (TwoFactorMethod.EmailCode, msg)

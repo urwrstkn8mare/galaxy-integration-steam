baca800fdc5ef1111697e3032857219d3d2a5cbb
baumhera <baumhera912@gmail.com>
2023-06-21 09:42:24 -0400

    update auto-copy-windows to use full path to the zip. Not sure why necessary but it's not a difficult fix.

--- auto-copy-windows.bat
+++ auto-copy-windows.bat
@@ -1,9 +1,12 @@
 @echo off
-setlocal
+setlocal enabledelayedexpansion
 cd /d %~dp0
 
 set "PLUGIN_PATH=%localappdata%\GOG.com\Galaxy\plugins\installed\steam_ca27391f-2675-49b1-92c0-896d43afa4f8"
 
 rmdir /S /Q -rf "%PLUGIN_PATH%"
 mkdir "%PLUGIN_PATH%"
-powershell "[Environment]::CurrentDirectory = Get-Location; Expand-Archive '.\windows.zip' -DestinationPath '%PLUGIN_PATH%'"
+
+set zip_file=%~dp0windows.zip
+echo Extracting %zip_file%
+powershell Expand-Archive '%zip_file%' -DestinationPath '%PLUGIN_PATH%'
